6db1fbc27ecbaa41fe3e421b5c3c5d7d403b03e6
Twistafries <twersterfrehs@gmail.com>
2018-04-10 02:41:06 +0800

    scripts revisions

--- app/appfx.js
+++ app/appfx.js
@@ -6,19 +6,6 @@ function showRad() {
 	a.style.display = a.style.display === 'block' ? 'none' : 'block';
 }
 
-function getForm(){
-	var inputs = document.getElementsByTagName('input');
-	var checked = [];
-	for (var i = 0; i < inputs.length; i++){
-		if(inputs[i].type == "checkbox"){
-			if(inputs[i].checked){
-				checked.push(inputs[i]);
-			}
-		}
-	}
-	return checked;
-}
-
 function hideMap() {
 	var canvas = document.getElementById("gMap");
 	var gen = document.getElementById("generate");
@@ -28,8 +15,8 @@ function hideMap() {
 	canvas.style.display = "none";
 }
 
+
 function drawMap() {
-	getForm();
 	var canvas = document.getElementById("gMap");
 	canvas.style.display = "block";
 	var context = gMap.getContext("2d");
@@ -42,58 +29,71 @@ function drawMap() {
 	hide.style.display = "block";
 	gen.style.display = "none";
 	
+	//container
 	context.rect(0, 0, x, y);
 	
-	//top and leftmost
-	context.rect(x*0.08, 0, x, y*0.05);
-	context.rect(0, y*0.04, x*0.05, y);
+	var sections = [
+		//top and leftmost
+		//context.rect(x*0.08, 0, x, y*0.05);
+		{"x":x*0.08, "y":0, "width":x, "length":y*0.05},
+		{"x":0, "y":y*0.04, "width":x*0.05, "length":y},
+		//1 - 6
+		{"x":x*0.08, "y":y*0.3, "width":x*0.06, "length":y*0.13},
+		{"x":x*0.16, "y":y*0.3, "width":x*0.06, "length":y*0.13},
+		{"x":x*0.08, "y":y*0.5, "width":x*0.06, "length":y*0.13},
+		{"x":x*0.16, "y":y*0.5, "width": x*0.06, "length":y*0.13},
+		{"x":x*0.08, "y":y*0.7, "width":x*0.06, "length":y*0.13},
+		{"x":x*0.16, "y":y*0.7, "width": x*0.06, "length":y*0.13},
+		
+		//top aisles
+		{"x":x*0.25, "y":y*0.16, "width":x*0.033, "length":y*0.3},
+		{"x":x*0.308, "y":y*0.16, "width":x*0.033, "length":y*0.3},
+		{"x":x*0.364, "y":y*0.16, "width":x*0.033, "length":y*0.3},
+		{"x":x*0.422, "y":y*0.16, "width":x*0.033, "length":y*0.3},
+		{"x":x*0.480, "y":y*0.16, "width":x*0.033, "length":y*0.3},
+		{"x":x*0.538, "y":y*0.16, "width":x*0.033, "length":y*0.3},
+		{"x":x*0.596, "y":y*0.16, "width":x*0.033, "length":y*0.3},
+		{"x":x*0.654, "y":y*0.16, "width":x*0.033, "length":y*0.3},
+		{"x":x*0.712, "y":y*0.16, "width":x*0.033, "length":y*0.3},
+		{"x":x*0.770, "y":y*0.16, "width":x*0.033, "length":y*0.3},
+		{"x":x*0.828, "y":y*0.16, "width":x*0.033, "length":y*0.3},
+		{"x":x*0.886, "y":y*0.16, "width":x*0.033, "length":y*0.3},
+		{"x":x*0.944, "y":y*0.16, "width":x*0.033, "length":y*0.3},
+		
+		//bot aisles
+		{"x":x*0.25, "y":y*0.5, "width":x*0.033, "length":y*0.3},
+		{"x":x*0.308, "y":y*0.5, "width":x*0.033, "length":y*0.3},
+		{"x":x*0.364, "y":y*0.5, "width":x*0.033, "length":y*0.3},
+		{"x":x*0.422, "y":y*0.5, "width":x*0.033, "length":y*0.3},
+		{"x":x*0.480, "y":y*0.5, "width":x*0.033, "length":y*0.3},
+		{"x":x*0.538, "y":y*0.5, "width":x*0.033, "length":y*0.3},
+		{"x":x*0.596, "y":y*0.5, "width":x*0.033, "length":y*0.3},
+		{"x":x*0.712, "y":y*0.5, "width":x*0.033, "length":y*0.3},
+		{"x":x*0.654, "y":y*0.5, "width":x*0.033, "length":y*0.3},
+		{"x":x*0.770, "y":y*0.5, "width":x*0.033, "length":y*0.3},
+		{"x":x*0.828, "y":y*0.5, "width":x*0.033, "length":y*0.3},
+		{"x":x*0.886, "y":y*0.5, "width":x*0.033, "length":y*0.3},
+		{"x":x*0.944, "y":y*0.5, "width":x*0.033, "length":y*0.3}
+	];
 	
-	//1 - 6
-	context.rect(x*0.08, y*0.3, x*0.06, y*0.13);
-	context.rect(x*0.16, y*0.3, x*0.06, y*0.13);
-	context.rect(x*0.08, y*0.5, x*0.06, y*0.13);
-	context.rect(x*0.16, y*0.5, x*0.06, y*0.13);
-	context.rect(x*0.08, y*0.7, x*0.06, y*0.13);
-	context.rect(x*0.16, y*0.7, x*0.06, y*0.13);
+	var inputs = document.getElementsByTagName('input');
 	
-	//top aisles
-	context.rect(x*0.25, y*0.16, x*0.033, y*0.3);
-	context.rect(x*0.308, y*0.16, x*0.033, y*0.3);
-	context.rect(x*0.364, y*0.16, x*0.033, y*0.3);
-	context.rect(x*0.422, y*0.16, x*0.033, y*0.3);
-	context.rect(x*0.480, y*0.16, x*0.033, y*0.3);
-	context.rect(x*0.538, y*0.16, x*0.033, y*0.3);
-	context.rect(x*0.596, y*0.16, x*0.033, y*0.3);
-	context.rect(x*0.654, y*0.16, x*0.033, y*0.3);
-	context.rect(x*0.712, y*0.16, x*0.033, y*0.3);
-	context.rect(x*0.770, y*0.16, x*0.033, y*0.3);
-	context.rect(x*0.828, y*0.16, x*0.033, y*0.3);
-	context.rect(x*0.886, y*0.16, x*0.033, y*0.3);
-	context.rect(x*0.944, y*0.16, x*0.033, y*0.3);
+	for(var i in sections){
+		context.rect(sections[i].x, sections[i].y, sections[i].width, sections[i].length);
+		for(var j = 0; j < inputs.length; j++){
+			if(inputs[j].checked && i == j){
+				context.fillStyle = '#91b4ff';
+				context.fillRect(sections[i].x, sections[i].y, sections[i].width, sections[i].length);
+			}
+		}
+	}
 	
+
 	
-	//bot aisles
-	context.rect(x*0.25, y*0.5, x*0.033, y*0.3);
-	context.rect(x*0.308, y*0.5, x*0.033, y*0.3);
-	context.rect(x*0.364, y*0.5, x*0.033, y*0.3);
-	context.rect(x*0.422, y*0.5, x*0.033, y*0.3);
-	context.rect(x*0.480, y*0.5, x*0.033, y*0.3);
-	context.rect(x*0.538, y*0.5, x*0.033, y*0.3);
-	context.rect(x*0.596, y*0.5, x*0.033, y*0.3);
-	context.rect(x*0.654, y*0.5, x*0.033, y*0.3);
-	context.rect(x*0.712, y*0.5, x*0.033, y*0.3);
-	context.rect(x*0.770, y*0.5, x*0.033, y*0.3);
-	context.rect(x*0.828, y*0.5, x*0.033, y*0.3);
-	context.rect(x*0.886, y*0.5, x*0.033, y*0.3);
-	context.rect(x*0.944, y*0.5, x*0.033, y*0.3);
-	
+	//paint
 	context.stroke();
-	
 	var rad = document.getElementById("rad");
 	rad.style.display = "none";
 	var a = document.getElementById("abt");
 	a.style.display = "block";
 }
-
-
-	
